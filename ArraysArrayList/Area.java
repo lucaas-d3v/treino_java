1f077b67a94d3cbcb862ed71545a5371d75c51e3
lucaspy-stack <lucas.workps.@gmail.com>
2025-07-21 22:45:07 -0300

    foi

--- ArraysArrayList/Area.java
+++ ArraysArrayList/Area.java
@@ -1,20 +1,3 @@
-/*
-Crie um programa que leia três valores de ponto flutuante: A, B e C. Em seguida, calcule e mostre:
-a) a área do triângulo retângulo que tem base A e altura C.
-b) a área do círculo de raio C. (pi = 3,14159)
-c) a área do trapézio que tem A e B pela base e C pela altura.
-d) a área do quadrado que tem lado B.
-e) a área do retângulo que tem lados A e B.
-
-Entrada
-O arquivo de entrada contém três valores double com um dígito após a vírgula decimal.
-
-Saída
-O arquivo de saída deve conter 5 linhas de dados. Cada linha corresponde a uma das áreas descritas acima,
-sempre com uma mensagem correspondente e um espaço entre os dois pontos e o valor. O valor calculado deve ser apresentado
- com 3 dígitos após a vírgula decimal.
-*/
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -40,32 +23,26 @@ public class Area {
         return (float)Math.pow(b, 2);
     }
 
-    public static float areaTrianguloRetangulo(float a, float b)
+    public static float areaTrianguloRetangulo(float a, float c)
     {
-        return (float)((a * b) / 2.0f);
+        return (a * c) / 2;
     }
 
     public static void main(String[] args)
     {
-        ArrayList<Float> numeros = new ArrayList<Float>();
-        String a;
         Scanner ler = new Scanner(System.in);
-
-        System.out.println("Digite o valor de a, b, c: ");
-        a = ler.nextLine();
-
-        for (String num : a.split(" "))
-        {
-            numeros.add((float) Integer.parseInt(num));
-        }
-
-        areaTrianguloRetangulo(numeros.get(0), numeros.get(1));
-        areaCirculo(numeros.get(2));
-        areaTrapezio(numeros.get(0), numeros.get(1), numeros.get(2));
-        areaQuadrado(numeros.get(1));
-        areaRetangulo(numeros.get(0), numeros.get(1));
-
+        String linha = ler.nextLine();
         ler.close();
 
+        String[] partes = linha.split(" ");
+        float a = Float.parseFloat(partes[0]);
+        float b = Float.parseFloat(partes[1]);
+        float c = Float.parseFloat(partes[2]);
+
+        System.out.printf("TRIANGULO: %.3f\n", areaTrianguloRetangulo(a, c));
+        System.out.printf("CIRCULO: %.3f\n", areaCirculo(c));
+        System.out.printf("TRAPEZIO: %.3f\n", areaTrapezio(a, b, c));
+        System.out.printf("QUADRADO: %.3f\n", areaQuadrado(b));
+        System.out.printf("RETANGULO: %.3f\n", areaRetangulo(a, b));
     }
 }
